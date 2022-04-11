63f93fac801b6041330023ff602789965e004d89
lpy134340 <lpy134340@gmail.com>
2022-04-11 10:30:14 -0400

    added tfidf and ML outline

--- LyricLibrary.py
+++ LyricLibrary.py
@@ -1,7 +1,8 @@
 from typing import List
 
 import pandas as pd
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 class LyricLibrary:
     """Lyric Library provides NLP tools for song analysis.
@@ -9,6 +10,10 @@ class LyricLibrary:
     """
     song_data = pd.DataFrame(columns=["Song", "Artist", "Genre", "Category", "Lyrics"])
     lyric_ngram_data = pd.DataFrame(columns=["Song", "1-Gram", "2-Gram", "3-Gram"])
+
+    # TODO: to change when we have the data
+    #lyrics = song_data['Lyrics']
+    lyrics = ["hello world hello", "what's up"]
 
     def load_song(self, song_name: str) -> None:
         # load song info from spotify, lyrics from genius
@@ -33,9 +38,39 @@ class LyricLibrary:
                 output.append(copy_current)
         return output
 
+    # create weights df for all the lyrics
+    def tfidf(self):
+        # TODO: double check if this is right
+
+        vect = TfidfVectorizer()
+        X = vect.fit_transform(self.lyrics)
+        print(vect.get_feature_names_out())
+        print(X.shape)
+
+        # get the weights
+        lyrics_weights = pd.DataFrame(X.toarray(), columns = vect.get_feature_names_out())
+        lyrics_weights["lyrics"] = self.lyrics
+        lyrics_weights.set_index("lyrics", inplace=True)
+        print(lyrics_weights)
+
+    def ML_pipeline(self, models):
+        # data loading
+
+        # train test split
+        # X_train, X_test, y_train, y_test = train_test_split(features, target, random_state=)
+
+        # run ML
+
+        # evaluate
+
+        return
+
+    def ngram_feature_extraction(self, genre, category):
+        return
 
 if __name__ == "__main__":
     library = LyricLibrary()
-    lyrics = ["the", "other", "day", "I", "did", "a", "thing", "and", "it", "was", "cool"]
-    n_grams = library.generate_ngrams(lyrics)
-    print(n_grams)
+    #lyrics = ["the", "other", "day", "I", "did", "a", "thing", "and", "it", "was", "cool"]
+    #n_grams = library.generate_ngrams(lyrics) # generate n-grams for one song?
+    #print(n_grams)
+    tfidf = library.tfidf()
